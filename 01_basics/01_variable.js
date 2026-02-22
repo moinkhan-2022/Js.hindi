eb76165cd0b1813387d0d5480c4fa014e9d055de
moin khan <moin.rafeek883@gmail.com>
2026-02-22 07:13:36 +0000

    Datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const account_ID=144553  //not changed
+let accountEmail="moin1234@gmail.com"
+var accountPassword="1234"
+accountCity="Agra"
+let accountState // if we dont declare then they give undefined
+
+//account_ID=2 // not chnage
+//console.log(accountID);
+//use variable let
+/*
+prefer not  to use var because of issue in block scope and fucntional scope 
+*/
+accountEmail="absnd"
+accountCity="jaipur"
+accountPasword="231"
+console.table([account_ID,accountEmail,accountPassword,accountCity,accountState])
+
