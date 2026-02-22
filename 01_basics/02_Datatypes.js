b37259d76483d44ed3db996e79fdd84ff57b7280
moin khan <moin.rafeek883@gmail.com>
2026-02-22 07:10:15 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,20 @@
+"use strict";// treat all JS code as newer version 
+//alert(3+3) // we are using not browser thats why error
+// console.log(3+3); 
+// console.log("moin"); // code readability important 
+let name="moin" //string
+let age =20 //int
+let isLOGGED=false //bool
+
+//number =>2 to power 53
+//bigint
+//string=""
+//boolean =true/false
+//null=stamd alone value 
+//undefined =
+//symbol=unique ke liye to eska use 
+//object 
+console.log(typeof null);
+console.log(typeof 123);
+console.log(typeof false);
+console.log(typeof "MOIn");
