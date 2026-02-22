19d9f9c924dc1ababa4d83732f8cf19896e9fb80
moin khan <moin.rafeek883@gmail.com>
2026-02-22 07:37:43 +0000

    Conversion

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,32 @@
+// console.log("moin");
+// //let score="33"
+// // let score=null
+// let score=true
+// //console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+
+
+// "33"=33
+// "33abc"=NaN
+// true=1;false=0
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1=true
+// 0=false
+// hitesh=true
+
+
+let number=33
+let stringNUM=String(number)
+console.log(stringNUM);
+console.log(typeof stringNUM);
+
+
+
